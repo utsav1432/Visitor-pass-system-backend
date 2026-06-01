d12df9abec07c68b2dc379c5b61ff00030b540fc
utsav1432 <utsavrathod78@gmail.com>
2026-06-01 19:03:53 +0530

    Fix appointment approval crash and Netlify routing

--- modules/SendEmail.js
+++ modules/SendEmail.js
@@ -1,15 +1,15 @@
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 const sendEmail = async (to, subject, message) => {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS
-      }
-    });
-
     const info = await transporter.sendMail({
       from: process.env.EMAIL,
       to,
